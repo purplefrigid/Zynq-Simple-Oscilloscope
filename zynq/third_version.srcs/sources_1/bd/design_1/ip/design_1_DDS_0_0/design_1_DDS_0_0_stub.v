09abaf3f3a0aa12ebe6aa57d3c7bf598ae9cc681
zjhstc <75159821+zjhstc@users.noreply.github.com>
2024-01-06 22:09:02 +0800

    Add files via upload

---
+++ zynq/third_version.srcs/sources_1/bd/design_1/ip/design_1_DDS_0_0/design_1_DDS_0_0_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+// Date        : Tue May 16 13:57:49 2023
+// Host        : DESKTOP-5J050JL running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top design_1_DDS_0_0 -prefix
+//               design_1_DDS_0_0_ design_1_DDS_0_1_stub.v
+// Design      : design_1_DDS_0_1
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "DDS,Vivado 2018.1" *)
+module design_1_DDS_0_0(clk, key, out_put, ad9708_clk)
+/* synthesis syn_black_box black_box_pad_pin="clk,key[3:0],out_put[7:0],ad9708_clk" */;
+  input clk;
+  input [3:0]key;
+  output [7:0]out_put;
+  output ad9708_clk;
+endmodule
